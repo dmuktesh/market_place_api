0a5af8dec1b32124d904f56c98b535d7d480638f
muktesh deshpande <muktesh@idyllic.co>
2017-03-15 20:27:58 +0530

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
-
 describe User do
   before { @user = FactoryGirl.build(:user) }
+describe "respond to" do
   subject { @user }
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid }
 end
+describe "when email is not present" do
+  before { @user.email = " " }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email)}
+end
+end
 =begin
-according to new rspec 
+according to new rspec
 require 'spec_helper'
 
 describe User do
@@ -18,6 +26,12 @@ describe User do
     expect(user).to respond_to(:email)
     expect(user).to respond_to(:password)
     expect(user).to respond_to(:password_confirmation)
+    expect(user).to be_valid
+  end
+end
+describe "when email is not present" do
+  let(:user) { FactoryGirl.build(:user, email: "") }
+  it "will invalidate user" do
     expect(user).to be_valid
   end
 end
