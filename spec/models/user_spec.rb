f1203e23f86c0f3f8b0db1160b0e1f1e1b03eed4
muktesh deshpande <muktesh@idyllic.co>
2017-03-15 18:55:32 +0530

    Adds user firsts specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+  subject { @user }
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should be_valid }
 end
+=begin
+according to new rspec 
+require 'spec_helper'
+
+describe User do
+  let(:user) { FactoryGirl.build(:user) }
+  it "" do
+    expect(user).to respond_to(:email)
+    expect(user).to respond_to(:password)
+    expect(user).to respond_to(:password_confirmation)
+    expect(user).to be_valid
+  end
+end
+=end
