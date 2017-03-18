3e9a9f571ac5dc9d4c6972b8a05e363925c9d69b
muktesh deshpande <muktesh@idyllic.co>
2017-03-18 11:14:21 +0530

    Adds the user create endpoint

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 gem 'pg'
+gem 'pry'
+gem 'pry-nav'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,6 @@
 #require 'rails_helper'
 require 'spec_helper'
+
 RSpec.describe Api::V1::UsersController, type: :controller do
   before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
   describe 'GET #show' do
@@ -12,5 +13,33 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(user_response[:email]).to eql @user.email
     end 
     it { should respond_with 200 } 
+  end
+  describe 'POST #create' do
+    context "when is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for(:user)
+        post :create, { user: @user_attributes }, format: :json
+      end
+      it "renders the json reepresentation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+      it { should respond_with 201 }
+    end
+    context "when is not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "test1234", password_confirmation: "test1234" }
+        post :create, {user: @invalid_user_attributes }, format: :json
+      end
+      it "renderrs an error json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+      it { should respond_with 422 }
+    end
   end
 end
