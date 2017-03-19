db68846cc28072b5702deb1a23cbdec172bb42fd
muktesh deshpande <muktesh@idyllic.co>
2017-03-19 17:07:16 +0530

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_uniqueness_of :email
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token!
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,50 @@
 require 'spec_helper'
 describe User do
-  before { @user = FactoryGirl.build(:user) }
-describe "respond to" do
-  subject { @user }
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
-  it { should be_valid }
+    before { @user = FactoryGirl.build(:user) }
+    describe 'respond to' do
+        subject { @user }
+        it { should respond_to(:email) }
+        it { should respond_to(:password) }
+        it { should respond_to(:password_confirmation) }
+        it { should be_valid }
+    end
+    describe 'when email is not present' do
+        before { @user.email = ' ' }
+        it { should validate_presence_of(:email) }
+        it { should validate_uniqueness_of(:email).case_insensitive }
+        it { should validate_confirmation_of(:password) }
+        it { should allow_value('example@domain.com').for(:email) }
+    end
+    it { should respond_to(:auth_token) }
+    it { should validate_uniqueness_of(:auth_token) }
+    describe "#generate_authentication_token!" do
+      it "generates a unique token" do
+        Devise.stub(:friendly_token).and_return("auniquetoken123")
+        @user.generate_authentication_token!
+        expect(@user.auth_token).to eql "auniquetoken123"
+      end
+      it 'genereates another token when on e already has been taken' do
+        existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+        @user.generate_authentication_token!
+        expect(@user.auth_token).not_to eql existing_user.auth_token
+      end
+    end
 end
-describe "when email is not present" do
-  before { @user.email = " " }
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
-  it { should validate_confirmation_of(:password) }
-  it { should allow_value('example@domain.com').for(:email)}
-end
-end
-=begin
-according to new rspec
-require 'spec_helper'
-
-describe User do
-  let(:user) { FactoryGirl.build(:user) }
-  it "" do
-    expect(user).to respond_to(:email)
-    expect(user).to respond_to(:password)
-    expect(user).to respond_to(:password_confirmation)
-    expect(user).to be_valid
-  end
-end
-describe "when email is not present" do
-  let(:user) { FactoryGirl.build(:user, email: "") }
-  it "will invalidate user" do
-    expect(user).to be_valid
-  end
-end
-=end
+# according to new rspec
+# require 'spec_helper'
+#
+# describe User do
+#   let(:user) { FactoryGirl.build(:user) }
+#   it "" do
+#     expect(user).to respond_to(:email)
+#     expect(user).to respond_to(:password)
+#     expect(user).to respond_to(:password_confirmation)
+#     expect(user).to be_valid
+#   end
+# end
+# describe "when email is not present" do
+#   let(:user) { FactoryGirl.build(:user, email: "") }
+#   it "will invalidate user" do
+#     expect(user).to be_valid
+#   end
+# end
